2f5aa244b2a4cc737ca6ab136a98b251b7d27035
BrunoVieiraSantana <brunonewdev@gmail.com>
2023-07-03 13:39:53 -0300

    restruturação de código

--- Desafio de projeto - Criando um Sistema Bancário com Python/main.py
+++ Desafio de projeto - Criando um Sistema Bancário com Python/main.py
@@ -26,57 +26,12 @@ while True:
     escolha = input('> ')
 
     if escolha == "1":
-        valor = float(input("Informe o valor do depósito: "))
-
-        if valor > 0:
-            saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}\n"
-            os.system('cls')
-            print("Operação realizada com sucesso!\n")
-
-
-        else:
-            os.system('cls')
-            print("Operação falhou! O valor informado é inválido.\n")
+        pass
 
     elif escolha == "2":
-        valor = float(input("Informe o valor do saque: "))
-
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= limite_saque
-
-        if excedeu_saldo:
-            os.system('cls')
-            print("Operação falhou! Você não tem saldo suficiente.\n")
-
-        elif excedeu_limite:
-            os.system('cls')
-            print("Operação falhou! O valor do saque excede o limite.\n")
-
-        elif excedeu_saques:
-            os.system('cls')
-            print("Operação falhou! Número máximo de saques excedido.\n")
-
-        elif valor > 0:
-            saldo -= valor
-            extrato += f"Saque: R$ {valor:.2f}\n"
-            numero_saques += 1
-            os.system('cls')
-            print("Operação realizada com sucesso!\n")
-
-        else:
-            os.system('cls')
-            print("Operação falhou! O valor informado é inválido.\n")
-
+        pass
     elif escolha == "3":
-        os.system('cls')
-        print("\n================ EXTRATO ================")
-        print("Não foram realizadas movimentações." if not extrato else extrato)
-        print(f"\nSaldo: R$ {saldo:.2f}")
-        print("==========================================\n")
+        pass
 
     elif escolha == "0":
         print("Agradecemos a preferência! Até mais!!!")
@@ -84,4 +39,4 @@ while True:
 
     else:
         os.system('cls')
-        print("Operação inválida, por favor selecione novamente a operação desejada.\n")
+        print('Operação invalida, tente novamente.')
