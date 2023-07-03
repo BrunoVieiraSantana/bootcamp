95006949cfa34544914416e0745456b093418987
BrunoVieiraSantana <brunonewdev@gmail.com>
2023-07-03 18:16:18 -0300

    estruturação

--- Desafio de projeto - Criando um Sistema Bancário com Python/main.py
+++ Desafio de projeto - Criando um Sistema Bancário com Python/main.py
@@ -1,10 +1,26 @@
 import os
 
-# Código utilizado para limpar o terminal
-os.system('cls')
+def criar_usuario(usuarios):
+    pass
 
-# Variável com as opções   
-menu= """
+def criar_conta(agencia, numero_conta, usuarios):
+    pass
+
+def menu_principal():
+    menu= """
+Digite o número referente à opção desejada:
+
+    [1] Criar Usuario
+    [2] Vincular conta
+    [3] Operações
+    [0] Sair
+"""
+    print (menu)
+    escolha = input('> ')
+    return escolha
+
+def menu_operacoes():
+    menu= """
 Digite o número referente à opção desejada:
 
     [1] Depósito
@@ -12,66 +28,99 @@ Digite o número referente à opção desejada:
     [3] Extrato
     [0] Sair
 """
-# Variáveis utilizadas para armazenar e definir limites 
-saldo = 0
-limite = 500
-extrato = ""
-numero_saques = 0
-limite_saque = 3
-
-# Loop de repetição, que exibe menu e opções que serão apresentadas ao usuário
-while True:
-    print('Sistema Bancário')
     print (menu)
     escolha = input('> ')
+    return escolha
 
-    if escolha == "1":
-        print('Digite o valor do deposito:')
-        valor = float(input('> '))
-        if valor > 0:
-            extrato+= f"Depósito: R$ {valor:.2f}\n"
-            saldo+=valor
-            os.system('cls')
-            print('Operação realizada com sucesso!\n')
-        else:
-            print('Digite um valor válido\n')
+def limpar_tela():
+    os.system('cls')
 
-
-    elif escolha == "2":
-
-        if numero_saques >= 3:
-            os.system('cls')
-            print('Número máximo de saques excedido') 
-        elif valor > 0:
-            print('Digite o valor do saque:')
-            valor = float(input('> '))
-            extrato+= f"Saque: R$ {valor:.2f}\n"
-            saldo-=valor
-            numero_saques+=1
-            os.system('cls')
-            print('Operação realizada com sucesso!\n')
-        else:
-            print('Digite um valor válido\n')
-
-
-    elif escolha == "3":
-        if len(extrato) == 0:
-            os.system('cls')
-            print('Nenhuma operação realizada.\n')
-        else:
-            os.system('cls')
-            print(f"{' - '*4}Extrato{' - '*4}")
-            print(extrato)
-            print(f"Saldo: R$ {saldo:.2f}")
-            print(f"{' - '*10}\n")
-
-
-    elif escolha == "0":
+def depositar(saldo, valor, extrato, /):
+    print('Digite o valor do deposito:')
+    valor = float(input('> '))
+    if valor > 0:
+        extrato+= f"Depósito: R$ {valor:.2f}\n"
+        saldo+=valor
         os.system('cls')
-        print("Agradecemos a preferência! Até mais!")
-        break
+        print('Operação realizada com sucesso!\n')
+    else:
+        print('Digite um valor válido\n')
 
+def saque(*, saldo, valor, extrato, limite, numero_saques, limite_saques):
+    if numero_saques >= 3:
+        os.system('cls')
+        print('Número máximo de saques excedido') 
+    elif valor > 0:
+        print('Digite o valor do saque:')
+        valor = float(input('> '))
+        extrato+= f"Saque: R$ {valor:.2f}\n"
+        saldo-=valor
+        numero_saques+=1
+        os.system('cls')
+        print('Operação realizada com sucesso!\n')
+    else:
+        print('Digite um valor válido\n')
 
+def exibir_extrato(saldo, /, *, extrato):
+    if len(extrato) == 0:
+        os.system('cls')
+        print('Nenhuma operação realizada.\n')
     else:
         os.system('cls')
-        print('Operação invalida, tente novamente.')
+        print(f"{' - '*4}Extrato{' - '*4}")
+        print(extrato)
+        print(f"Saldo: R$ {saldo:.2f}")
+        print(f"{' - '*10}\n")
+
+def main():
+    agencia = '0001'
+    saldo = 0
+    limite = 500
+    extrato = ""
+    numero_saques = 0
+    limite_saque = 3
+    usuarios = []
+    contas = []
+    print('Sistema Bancário')
+    while True:
+        limpar_tela()
+        escolha_principal = menu_principal()
+        if escolha_principal == "1":
+            saque()
+
+        elif escolha_principal == "2":
+            depositar()
+
+        elif escolha_principal == "3":
+            os.system('cls')
+            escolha = menu_operacoes()
+            if escolha == "1":
+                saque()
+
+            elif escolha == "2":
+                depositar()
+
+            elif escolha == "3":
+                exibir_extrato()
+
+            elif escolha == "0":
+                limpar_tela()
+                print("Agradecemos a preferência! Até mais!")
+                break
+
+            else:
+                limpar_tela()
+                print('Operação invalida, tente novamente.')
+
+        elif escolha_principal == "0":
+            limpar_tela()
+            print("Agradecemos a preferência! Até mais!")
+            break
+
+        else:
+            limpar_tela()
+            print('Operação invalida, tente novamente.')
+        
+
+
+main()
